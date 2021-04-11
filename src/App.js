8dce0748fb794352e4227d27c9c869b54b9cc31b
ankanr <zeekozk9@gmail.com>
2021-04-11 18:40:18 +0530

    dropdown made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.css";
+import DropDown from "./Components/DropDown";
 
 const App = () => {
-  return <h1>Edvicer</h1>;
+  const options = [
+    {
+      label: "Machine Learning",
+      value: "ml",
+    },
+    {
+      label: "Data Structures",
+      value: "dsa",
+    },
+    {
+      label: "Java",
+      value: "jv",
+    },
+    {
+      label: "Python",
+      value: "py",
+    },
+  ];
+
+  const [course, setCourse] = useState(options[0]);
+  return (
+    <div className='header'>
+      <h1>What are you skilled in ?</h1>
+      <DropDown
+        selected={course}
+        onSelectedChange={setCourse}
+        options={options}
+        label='Select a course'
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,1 +1,62 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+const DropDown = ({ selected, onSelectedChange, options, label }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current === null || ref.current.contains(event.target)) {
+        return;
+      }
+
+      setOpen(false);
+    };
+
+    document.body.addEventListener("click", onBodyClick);
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
+    return (
+      <div
+        class='item'
+        key={option.value}
+        className='item'
+        onClick={() => onSelectedChange(option)}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div ref={ref} className='ui form'>
+      <div className='field'>
+        <label className='label'>{label}</label>
+        <div
+          onClick={() => setOpen(!open)}
+          class={`ui fluid search selection dropdown ${
+            open ? "visible active" : ""
+          }`}
+        >
+          <input type='hidden' name='country' />
+          <i class='dropdown icon'></i>
+          <div className='text'>{selected.label}</div>
+          <div class={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DropDown;
